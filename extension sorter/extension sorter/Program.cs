2fa2053705a4fc82a89f967f2af89bbdc0dc9b2a
Arsalan Peerzada <peerzada_1994@live.com>
2019-03-24 09:33:22 +0500

    Initial Commit

---
+++ extension sorter/extension sorter/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace extension_sorter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\t \t" + "Desktop Manager");
+            bool bol = false;
+            string p = @"C:\Users\Wali\Downloads\Desktop";//the link jahan se files utha rhe hain
+            do//use do for repeating process
+            {
+                int Length = 30;
+                Console.Write("Extension:  ");
+                string ex = Console.ReadLine();//extension user defined kara
+
+                string[] d = Directory.GetFiles(p, "*." + ex);//then created directory and retrive files in array d
+                Console.WriteLine(@"Create NewFolder or Use Existing folder
+
+Press 1 to New Folder
+
+Press 2 for Existing Folder");
+                string file = Console.ReadLine().ToLower();//use if for new folder aur not
+                if (file == "1".ToString())
+                {
+                    Console.WriteLine(@"Path format= c:\example\");
+                    Console.Write("Path:   ");
+                    string path = @"C:\Users\Arsalan peerzada\Downloads\Desktop\New";
+
+                    Directory.CreateDirectory(path);
+                    Console.WriteLine("Folder Created");
+
+                    split1 obj = new split1();
+                    obj.splitNew(Length, d, path);
+                }
+                else if (file == "2".ToString())
+                {
+                    Console.WriteLine(@"Path format= c:\example\");
+                    Console.Write("Path:   ");
+                    string path = Console.ReadLine();
+
+                    split1 obj = new split1();
+                    obj.splitNew(Length, d, path);
+                }
+
+                Console.WriteLine("Move more files Enter Yes OR No");//in this part programe ask for repeat aur not
+                string next = Console.ReadLine().ToString();
+                if (next == "yes")
+                {
+                    bol = true; //yes pe bool ko true karde ga aur 
+                }
+                else
+                {
+                    break;
+                }
+
+            } while (bol == true);
+        }
+    }
+}
---
+++ extension sorter/extension sorter/split1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace extension_sorter
+{
+    class split1
+    {
+        public void splitNew(int _Length, string[] _d, string _path)
+        {
+            string[] extension = new string[_Length+1]; //taken array for further use 
+
+            string[] trim = new string[_Length+1]; //taken array for further use
+            int l = 0;// use new variable because we can not use d[] in for{j} cuz for renew it. we want existing varialbe
+            foreach (string item in _d)
+            {
+                trim = item.Split(new char[] { '\\'}, StringSplitOptions.RemoveEmptyEntries); // split use \\ for split directory
+
+                
+                for (int j = 0; j < trim.Length; j++)
+                {
+                    
+                    if (trim[j] == "Desktop")
+                    {
+                        extension[j] = trim[j + 1];   // save next array element of Desktop that is our desired file
+                        Console.WriteLine(extension[j]); // print that file
+
+                        File.Move(_d[l], @_path + extension[j]); //then move file d represent previous path, path+extention shows new
+                        l++;                                                               //its like tm naraz ho+mp3
+                    }
+
+                }
+            }
+
+        }
+    }
+}
